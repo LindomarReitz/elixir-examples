f5fff023492c170bbd680b96b6e33059c39a96bd
Lindomar Reitz <lindomar.reitz@gmail.com>
2019-06-01 20:52:06 +0100

    Add geometry tests

--- test/geometry_test.exs
+++ test/geometry_test.exs
@@ -1,12 +1,12 @@
 defmodule GeometryTest do
-    use ExUnit.Case
-    doctest Geometry
+  use ExUnit.Case
+  doctest Geometry
 
-    test "returns the area" do
-        assert Geometry.rectangle_area(5, 6) == 30
-    end
+  test "returns the area" do
+    assert Geometry.rectangle_area(5, 6) == 30
+  end
 
-    test "returns zero" do
-        assert Geometry.rectangle_area(6) == 0
-    end
+  test "returns zero" do
+    assert Geometry.rectangle_area(6) == 0
+  end
 end

a22e99c0d2c8fdaaa81d5862804861c67ef635b7
Lindomar Reitz <lindomar.reitz@gmail.com>
2019-06-01 17:19:37 +0100

    Add mix project structure

---
+++ lib/Geometry.ex
@@ -0,0 +1,5 @@
+defmodule Geometry do
+  def rectangle_area(a \\ 0, b) do
+    a * b
+  end
+end
---
+++ test/geometry_test.exs
@@ -0,0 +1,12 @@
+defmodule GeometryTest do
+    use ExUnit.Case
+    doctest Geometry
+
+    test "returns the area" do
+        assert Geometry.rectangle_area(5, 6) == 30
+    end
+
+    test "returns zero" do
+        assert Geometry.rectangle_area(6) == 0
+    end
+end
